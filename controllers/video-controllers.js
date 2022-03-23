658bf60f2f73b71e0769cdbb079aa55f5620f61a
Jorge-Figueira <xurxo.figueira@gmail.com>
2022-03-23 15:25:44 +0000

    Adds test for negative votes on the endpoint PATCH /api/videos/:video_id

--- controllers/video-controllers.js
+++ controllers/video-controllers.js
@@ -1,13 +1,15 @@
-
-const { fetchVideos, fetchVideoById, addVideo, patchVotesByVideoId } = require("../models/video-models");
-
+const {
+  fetchVideos,
+  fetchVideoById,
+  addVideo,
+  patchVotesByVideoId,
+} = require("../models/video-models");
 
 exports.getVideos = (req, res) => {
   fetchVideos().then((videos) => {
     res.status(200).send({ videos });
   });
 };
-
 
 exports.updateVotesByVideoId = (req, res, next) => {
   const vote = req.body.vote;
@@ -16,23 +18,26 @@ exports.updateVotesByVideoId = (req, res, next) => {
   patchVotesByVideoId({ video_id, vote })
     .then((video) => {
       res.status(200).send(video);
-
-
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 exports.getVideoById = (req, res, next) => {
-  const {video_id} = req.params;
+  const { video_id } = req.params;
 
   fetchVideoById(video_id)
-  .then((video) => {
-    res.status(200).send({video})
-  }).catch(next)
-}
+    .then((video) => {
+      res.status(200).send({ video });
+    })
+    .catch(next);
+};
 
 exports.postVideo = (req, res, next) => {
   const { title, username, description, cloudinary_id } = req.body;
   addVideo(title, username, description, cloudinary_id)
     .then((body) => {
       res.status(201).send({ postedVideo: body[0] });
-
     })
     .catch((err) => {
       next(err);

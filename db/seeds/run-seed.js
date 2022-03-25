fb5e938f74a9002c911af1b08cf2f74a503b024e
David Clark <david.p.t.clark@gmail.com>
2022-03-25 09:53:06 +0000

    Add development database

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,9 +1,9 @@
-const testData = require("../test-data/index.js");
+const devData = require("../dev-data/index.js"); //Change to reflect index file in dev-data dir.
 const seed = require("./seed.js");
 const db = require("../connection.js");
 
 const runSeed = () => {
-  return seed(testData).then(() => db.end());
+  return seed(devData).then(() => db.end()); //Seed using new variable linking to dev-data
 };
 
 runSeed();

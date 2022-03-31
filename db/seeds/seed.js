2a457005a210bfbd1c240721b6829382a988d60e
Tom wallwin <TWallwin>
2022-03-31 10:57:11 +0100

    added if statements to sql inserts

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -12,68 +12,89 @@ const seed = async ({
 }) => {
   await dropTables();
   await createTables();
+  if (userData.length > 0) {
+    const insertUsersQueryStr = format(
+      "INSERT INTO users (username, avatar_url, bio, type, social_url,password) VALUES %L RETURNING *;",
+      userData.map(
+        ({ username, avatar_url, bio, type, social_url, password }) => {
+          return [username, avatar_url, bio, type, social_url, password];
+        },
+      ),
+    );
 
-  const insertUsersQueryStr = format(
-    "INSERT INTO users (username, avatar_url, bio, type, social_url,password) VALUES %L RETURNING *;",
-    userData.map(
-      ({ username, avatar_url, bio, type, social_url, password }) => {
-        return [username, avatar_url, bio, type, social_url, password];
-      },
-    ),
-  );
+    await db.query(insertUsersQueryStr).then((result) => {
+      return result.rows;
+    });
+  }
+  if (tagsData.length > 0) {
+    const insertTagsQueryStr = format(
+      "INSERT INTO tags (tag) VALUES %L RETURNING *;",
+      tagsData.map(({ tag }) => [tag]),
+    );
 
-  await db.query(insertUsersQueryStr).then((result) => {
-    return result.rows;
-  });
-  const insertTagsQueryStr = format(
-    "INSERT INTO tags (tag) VALUES %L RETURNING *;",
-    tagsData.map(({ tag }) => [tag]),
-  );
-  await db.query(insertTagsQueryStr).then((result) => {
-    return result.rows;
-  });
+    await db.query(insertTagsQueryStr).then((result) => {
+      return result.rows;
+    });
+  }
+  if (videoData.length > 0) {
+    const convertedVideoData = videoData.map((video) =>
+      convertTimestampToDate(video),
+    );
 
-  const convertedVideoData = videoData.map((video) =>
-    convertTimestampToDate(video),
-  );
+    const insertVideosQueryStr = format(
+      "INSERT INTO videos (title, username, created_at, votes, description, cloudinary_id) VALUES %L RETURNING *;",
+      convertedVideoData.map(
+        ({
+          title,
+          username,
+          created_at,
+          votes,
+          description,
+          cloudinary_id,
+        }) => {
+          return [
+            title,
+            username,
+            created_at,
+            votes,
+            description,
+            cloudinary_id,
+          ];
+        },
+      ),
+    );
+    await db.query(insertVideosQueryStr).then((result) => {
+      return result.rows;
+    });
+  }
+  if (commentData.length > 0) {
+    const convertedCommentData = commentData.map((comment) =>
+      convertTimestampToDate(comment),
+    );
 
-  const insertVideosQueryStr = format(
-    "INSERT INTO videos (title, username, created_at, votes, description, cloudinary_id) VALUES %L RETURNING *;",
-    convertedVideoData.map(
-      ({ title, username, created_at, votes, description, cloudinary_id }) => {
-        return [title, username, created_at, votes, description, cloudinary_id];
-      },
-    ),
-  );
-  await db.query(insertVideosQueryStr).then((result) => {
-    return result.rows;
-  });
+    const insertCommentsQueryStr = format(
+      "INSERT INTO comments (body, username, video_id, created_at) VALUES %L RETURNING *;",
+      convertedCommentData.map(({ body, username, video_id, created_at }) => {
+        return [body, username, video_id, created_at];
+      }),
+    );
 
-  const convertedCommentData = commentData.map((comment) =>
-    convertTimestampToDate(comment),
-  );
+    await db.query(insertCommentsQueryStr).then((result) => {
+      return result.rows;
+    });
+  }
+  if (tags_videosData.length > 0) {
+    const insertTagsVideosQueryStr = format(
+      "INSERT INTO tags_videos (video_id, tag) VALUES %L RETURNING *;",
+      tags_videosData.map(({ videoId, tag }) => {
+        return [videoId, tag];
+      }),
+    );
 
-  const insertCommentsQueryStr = format(
-    "INSERT INTO comments (body, username, video_id, created_at) VALUES %L RETURNING *;",
-    convertedCommentData.map(({ body, username, video_id, created_at }) => {
-      return [body, username, video_id, created_at];
-    }),
-  );
-
-  await db.query(insertCommentsQueryStr).then((result) => {
-    return result.rows;
-  });
-
-  const insertTagsVideosQueryStr = format(
-    "INSERT INTO tags_videos (video_id, tag) VALUES %L RETURNING *;",
-    tags_videosData.map(({ videoId, tag }) => {
-      return [videoId, tag];
-    }),
-  );
-
-  await db.query(insertTagsVideosQueryStr).then((result) => {
-    return result.rows;
-  });
+    await db.query(insertTagsVideosQueryStr).then((result) => {
+      return result.rows;
+    });
+  }
 };
 
 module.exports = seed;

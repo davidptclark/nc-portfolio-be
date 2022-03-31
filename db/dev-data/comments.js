96a6cd9d595ef64127316c70e872c744285776fe
Tom wallwin <TWallwin>
2022-03-31 10:33:16 +0100

    set data for final reseed

--- db/dev-data/comments.js
+++ db/dev-data/comments.js
@@ -1,41 +1,40 @@
 module.exports = [
-  {
-    body: "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
-    username: "butter_bridge",
-    video_id: "gtlfsn9vw27jlymb7uqt",
-    created_at: 1604113380000,
-  },
-  {
-    body: "Delicious crackerbreads",
-    username: "icellusedkars",
-    video_id: "gtlfsn9vw27jlymb7uqt",
-    created_at: 1586899140000,
-  },
-  {
-    body: "Superficially charming",
-    username: "icellusedkars",
-    video_id: "ecxfpaqusbwohr3ryboy",
-    created_at: 1577848080000,
-  },
-  {
-    body: "git push origin master",
-    username: "icellusedkars",
-    video_id: "ecxfpaqusbwohr3ryboy",
-    created_at: 1592641440000,
-  },
-  {
-    body: "Ambidextrous marsupial",
-    username: "icellusedkars",
-    video_id: "e4te4nxsiu5zvmthqtj0",
-    created_at: 1600560600000,
-  },
-
-  {
-    body: "What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.",
-    username: "icellusedkars",
-    video_id: "an6wcgewiwycopr38aod",
-    created_at: 1591682400000,
-  },
+  // {
+  //   body: "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
+  //   username: "butter_bridge",
+  //   video_id: "gtlfsn9vw27jlymb7uqt",
+  //   created_at: 1604113380000,
+  // },
+  // {
+  //   body: "Delicious crackerbreads",
+  //   username: "icellusedkars",
+  //   video_id: "gtlfsn9vw27jlymb7uqt",
+  //   created_at: 1586899140000,
+  // },
+  // {
+  //   body: "Superficially charming",
+  //   username: "icellusedkars",
+  //   video_id: "ecxfpaqusbwohr3ryboy",
+  //   created_at: 1577848080000,
+  // },
+  // {
+  //   body: "git push origin master",
+  //   username: "icellusedkars",
+  //   video_id: "ecxfpaqusbwohr3ryboy",
+  //   created_at: 1592641440000,
+  // },
+  // {
+  //   body: "Ambidextrous marsupial",
+  //   username: "icellusedkars",
+  //   video_id: "e4te4nxsiu5zvmthqtj0",
+  //   created_at: 1600560600000,
+  // },
+  // {
+  //   body: "What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.",
+  //   username: "icellusedkars",
+  //   video_id: "an6wcgewiwycopr38aod",
+  //   created_at: 1591682400000,
+  // },
   // {
   //   body: "I am 100% sure that we're not completely sure.",
   //   username: "butter_bridge",
@@ -60,7 +59,6 @@ module.exports = [
   //   video_id: "gammf8exdeo05xlmsq2j",
   //   created_at: 1595294400000,
   // },
-
   // {
   //   body: "The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.",
   //   username: "butter_bridge",
@@ -91,7 +89,6 @@ module.exports = [
   //   video_id: "egjsgg7p4bf8jfszzx9e",
   //   created_at: 1600560600000,
   // },
-
   // {
   //   body: "What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.",
   //   username: "icellusedkars",
